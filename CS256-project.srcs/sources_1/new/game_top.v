00181543fdcf9fca8816c869a604bd6fc1e0280f
MForystek <micforystek@gmail.com>
2023-11-18 02:34:55 +0300

    update

--- CS256-project.srcs/sources_1/new/game_top.v
+++ CS256-project.srcs/sources_1/new/game_top.v
@@ -119,8 +119,8 @@ module bullet #(parameter FROM_CANNON = 0, parameter BULLET_NUM = 0)(
     output reg [10:0] bullet_pos_x, output [9:0] bullet_pos_y
     );
     
-    localparam [6:0] DELAY = 7'd66 / `BULLETS_PER_CANNON * BULLET_NUM;
-    reg [6:0] bullet_timer;
+    localparam [5:0] DELAY = 6'd60 / `BULLETS_PER_CANNON * BULLET_NUM;
+    reg [5:0] bullet_timer;
     
     always @ (posedge logclk) begin
         if (!rst || !cannons_on[FROM_CANNON])
@@ -129,7 +129,8 @@ module bullet #(parameter FROM_CANNON = 0, parameter BULLET_NUM = 0)(
             bullet_timer <= bullet_timer + 1'b1;
         
         if (!rst || bullet_pos_x >= `WIDTH - `FRAME_WIDTH - 11'd1 
-            || (!cannons_on[FROM_CANNON] && bullet_timer < DELAY && bullet_pos_x == `CANNON_OFFSET_X + `CANNON_WIDTH - `BULLET_WIDTH))
+            || (bullet_pos_x == `CANNON_OFFSET_X + `CANNON_WIDTH - `BULLET_WIDTH 
+                && (!cannons_on[FROM_CANNON] || (cannons_on[FROM_CANNON] && bullet_timer < DELAY))))
             bullet_pos_x <= `CANNON_OFFSET_X + `CANNON_WIDTH - `BULLET_WIDTH;
         else
             bullet_pos_x <= bullet_pos_x + `BULLET_SPEED;
